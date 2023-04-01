b1671233adeb70bd9dbc944da0f6e3a2c658d743
Daulet1424 <129593504+Daulet1424@users.noreply.github.com>
2023-04-01 17:07:14 +0600

    Update bot2.py

--- bot2.py
+++ bot2.py
@@ -1,31 +1,65 @@
-import telebot
-from telebot import types
+import requests
+import datetime
+from config import tg_bot_token, open_weather_token
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
 
 
-bot = telebot.TeleBot('6253766259:AAEfxrASAV64LVLylvalySGHUISFBghBJlI')
+bot = Bot(token=tg_bot_token)
+dp = Dispatcher(bot)
 
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    mess = f'Привет, <b>{message.from_user.first_name}</b> <u><b>{message.from_user.last_name}</b></u>'
-    bot.send_message(message.chat.id, mess, parse_mode='html')
-
-@bot.message_handler(commands=['website'])
-def bot_message(message):
-    markup = types.InlineKeyboardMarkup()
-    markup.add(types.InlineKeyboardButton("Посетите сайт", url='https://brainius.kz/'))
-    bot.send_message(message.chat.id, 'Перейдите на сайт', reply_markup=markup)
-
-@bot.message_handler(commands=['help'])
-def bot_message(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
-    website = types.KeyboardButton('Веб сайт')
-    start = types.KeyboardButton('Start')
-    markup.add(website, start)
-    bot.send_message(message.chat.id, 'Перейдите на сайт', reply_markup=markup)
-    if website == True:
-        bot.send_message(message.chat.id, 'Формулы', url = 'https://brainius.kz/')
-bot.polling(none_stop=True)
+@dp.message_handler(commands=["start"])
+async def start_command(message: types.Message):
+    await message.reply("Привет! Напиши мне название города и я пришлю сводку погоды!")
 
 
+@dp.message_handler()
+async def get_weather(message: types.Message):
+    code_to_smile = {
+        "Clear": "Ясно \U00002600",
+        "Clouds": "Облачно \U00002601",
+        "Rain": "Дождь \U00002614",
+        "Drizzle": "Дождь \U00002614",
+        "Thunderstorm": "Гроза \U000026A1",
+        "Snow": "Снег \U0001F328",
+        "Mist": "Туман \U0001F32B"
+    }
 
+    try:
+        r = requests.get(
+            f"http://api.openweathermap.org/data/2.5/weather?q={message.text}&appid={open_weather_token}&units=metric"
+        )
+        data = r.json()
+
+        city = data["name"]
+        cur_weather = data["main"]["temp"]
+
+        weather_description = data["weather"][0]["main"]
+        if weather_description in code_to_smile:
+            wd = code_to_smile[weather_description]
+        else:
+            wd = "Посмотри в окно, не пойму что там за погода!"
+
+        humidity = data["main"]["humidity"]
+        pressure = data["main"]["pressure"]
+        wind = data["wind"]["speed"]
+        sunrise_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunrise"])
+        sunset_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunset"])
+        length_of_the_day = datetime.datetime.fromtimestamp(data["sys"]["sunset"]) - datetime.datetime.fromtimestamp(
+            data["sys"]["sunrise"])
+
+        await message.reply(f"***{datetime.datetime.now().strftime('%Y-%m-%d %H:%M')}***\n"
+              f"Погода в городе: {city}\nТемпература: {cur_weather}C° {wd}\n"
+              f"Влажность: {humidity}%\nДавление: {pressure} мм.рт.ст\nВетер: {wind} м/с\n"
+              f"Восход солнца: {sunrise_timestamp}\nЗакат солнца: {sunset_timestamp}\nПродолжительность дня: {length_of_the_day}\n"
+              f"***Хорошего дня!***"
+              )
+
+    except:
+        await message.reply("\U00002620 Проверьте название города \U00002620")
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
